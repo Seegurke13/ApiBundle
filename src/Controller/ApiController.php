9b1a5e0d0abb4cc67807156df7662a688ea63e43
Jerome Trope <jerometrope@gmail.com>
2020-09-22 12:34:52 +0200

    fix case error

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -32,15 +32,40 @@ class ApiController
 
     public function get(string $classname, int $id)
     {
-        return new Response($this->serializer->serialize($this->entityManager->getRepository($classname)->find($id), 'json'));
+        return new Response($this->serializer->serialize($this->entityManager->getRepository($classname)->find($id), 'json', [
+            'circular_reference_handler' => function ($object) {
+                return $object->getId();
+            }
+        ]));
     }
 
-    public function list(string $className)
+    public function list(string $classname, Request $request)
     {
-        $repo = $this->entityManager->getRepository($className);
-        $data = $repo->findAll();
+        $repo = $this->entityManager->getRepository($classname);
+        /** @var QueryBuilder $qb */
+        $qb = $repo->createQueryBuilder('a');
 
-        return new Response($this->serializer->serialize($data, 'json'));
+        if($request->query->has('ordering')) {
+            $order = $request->query->has('desc') ? 'DESC': 'ASC';
+            $qb->orderBy('a.' .$request->query->get('ordering'), $order);
+            $request->query->remove('ordering');
+            $request->query->remove('desc');
+        }
+
+        if ($request->query->has('p')) {
+            $count = $request->query->has('c') ? $request->query->get('c') : 50;
+            $qb->setFirstResult(intval($request->query->get('p')) * $count);
+            $qb->setMaxResults($count);
+            $request->query->remove('p');
+            $request->query->remove('c');
+        }
+        $data = $qb->getQuery()->getResult();
+
+        return new Response($this->serializer->serialize($data, 'json', [
+            'circular_reference_handler' => function ($object) {
+                return $object->getId();
+            }
+        ]));
     }
 
     public function create(Request $request, string $classname)
@@ -76,6 +101,21 @@ class ApiController
         /** @var QueryBuilder $qb */
         $qb = $repository->createQueryBuilder('a');
 
+        if($request->query->has('ordering')) {
+            $order = $request->query->has('desc') ? 'DESC': 'ASC';
+            $qb->orderBy('a.' .$request->query->get('ordering'), $order);
+            $request->query->remove('ordering');
+            $request->query->remove('desc');
+        }
+
+        if ($request->query->has('p')) {
+            $count = $request->query->has('c') ? $request->query->get('c') : 50;
+            $qb->setFirstResult(intval($request->query->get('p')) * $count);
+            $qb->setMaxResults($count);
+            $request->query->remove('p');
+            $request->query->remove('c');
+        }
+
         if($request->query->has('q') && isset($this->entityManager->getClassMetadata($classname)->fieldMappings['name'])) {
             $qb->where('a.name LIKE :key');
             $qb->setParameter('key','%'.$request->query->get('q'). '%');
@@ -87,6 +127,10 @@ class ApiController
             }
         }
 
-        return new Response($this->serializer->serialize($qb->getQuery()->getResult(), 'json'));
+        return new Response($this->serializer->serialize($qb->getQuery()->getResult(), 'json', [
+            'circular_reference_handler' => function ($object) {
+                return $object->getId();
+            }
+        ]));
     }
 }
